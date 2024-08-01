ef864a740c3d4caeb92238da25c43622a6a1c8b9
kellystephany24 <138157065+kellystephany24@users.noreply.github.com>
2024-08-01 14:18:44 -0600

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/kellystephany24/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('kellystephany24')
+            node / gitConfigEmail('kellystephany24@yahoo.com')
         }
     }
     triggers {
